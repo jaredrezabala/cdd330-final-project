69978225cb515344bac5c266690957a21a588949
jaredrezabala <113398618+jaredrezabala@users.noreply.github.com>
2024-12-04 20:45:59 -0500

    initial set up

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from "path";
+// eslint-disable-next-line import/namespace
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+      },
+    },
+  },
+});
